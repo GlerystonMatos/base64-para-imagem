95e6f382ad9faf2b5bc5c077bf172253e84971eb
Gleryston Matos <glerystonmatos@yahoo.com.br>
2020-08-17 14:40:26 -0300

    Criação de opção para selecionar imagem e exemplo de encode para base 64.

--- Fontes/uPrincipal.pas
+++ Fontes/uPrincipal.pas
@@ -5,14 +5,19 @@ interface
 uses
   Winapi.Windows, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
   Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.Buttons, Soap.EncdDecd,
-  Vcl.ExtCtrls, JPEG;
+  Vcl.ExtCtrls, JPEG, Vcl.ExtDlgs, IdCoderMIME;
 
 type
   TfrmPrincipal = class(TForm)
     btnConverter: TBitBtn;
     Image: TImage;
     Memo: TMemo;
+    btnSelecionar: TBitBtn;
+    opdImage: TOpenPictureDialog;
+    btnLimpar: TBitBtn;
     procedure btnConverterClick(Sender: TObject);
+    procedure btnSelecionarClick(Sender: TObject);
+    procedure btnLimparClick(Sender: TObject);
   private
     { Private declarations }
   public
@@ -49,6 +54,33 @@ begin
     end;
   finally
     FreeAndNil(jpegImage);
+  end;
+end;
+
+procedure TfrmPrincipal.btnLimparClick(Sender: TObject);
+begin
+  Memo.Lines.Clear;
+end;
+
+procedure TfrmPrincipal.btnSelecionarClick(Sender: TObject);
+var
+  stream: TFileStream;
+  base64: TIdEncoderMIME;
+begin
+  opdImage.InitialDir := ExtractFileDir(Application.ExeName) + '\Imagen\';
+  if (opdImage.Execute) then
+  begin
+    base64 := TIdEncoderMIME.Create(nil);
+    try
+      stream := TFileStream.Create(opdImage.FileName, fmOpenRead);
+      try
+        Memo.Lines.Text := TIdEncoderMIME.EncodeStream(stream);
+      finally
+        FreeAndNil(stream);
+      end;
+    finally
+      FreeAndNil(base64);
+    end;
   end;
 end;
 
